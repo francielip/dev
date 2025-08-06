6e0885077496d623d5ec28fead045e419fc3a5af
Francieli A. Persch <francieli.p@elian.com.br>
2025-08-06 12:27:29 -0300

    Alterações dos arquivos para aula de JS.

---
+++ Html - CSS - JS/Curso JS/Aula 02 - Tipos de saídas/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto <strong>JS</strong>";
+//pegando o elemento da página html que tenha o ID = texto e inserindo um conteúdo nele
+alert(10+5);
+console.log("Isso é um log...");
